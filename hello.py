0455290ee240c04ef7e6789f33336389246aaef9
shagun9525-code <shagun9525@gmail.com>
2026-08-18 08:27:01 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
 print("hello,Github")
+print("today I am learning bioinformatics")
+# bioinformatics 
+## defination
+- bioinfo is the branch of science that combines biology,computational science,information technology,mathematics and statistics to collect,organise,store,analysis and interpretation of biological information 
+
+-it is the application of computer and computational technique to solve biological problems
+
+## need for bioinformatics 
